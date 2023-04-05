0a88f8018af85842f5ccfbbd80e69cd1f9d99993
Matthew Hoang <hoangmatthew1999@gmail.com>
2023-04-04 18:41:37 -0700

    added a grid

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,17 @@ class FirstPage extends StatelessWidget{
     return Scaffold(appBar: AppBar(
       title: Text("CMPE 137 Project 1"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text("On the second page ")), 
-        ),
+      body: GridView.count(
+  // Create a grid with 2 columns. If you change the scrollDirection to
+  // horizontal, this produces 2 rows.
+  crossAxisCount: 3,
+  // Generate 100 widgets that display their index in the List.
+  children: List.generate(9, (index) {
+    return Center(
+      child: ElevatedButton(onPressed: () {print(index);}, child: Text("Click to add position ")),
+    );
+  }),
+),
       );
   }
 }
