dfa6ba432e1bd12972dd6d5ef0d4fa679e95e77e
Matthew Hoang <hoangmatthew1999@gmail.com>
2023-04-10 13:33:20 -0700

    finished the tic tac toe button pressing

--- lib/main.dart
+++ lib/main.dart
@@ -51,18 +51,37 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  String x_or_o = "";
+  List<String> x_or_o_Array = ["", "","","","","","","","",];
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: Text("CMPE 137 Project 1"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text("Click to play")), 
-        ),
-      );;
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(appBar: AppBar(title: Text("CMPE 137 Project 1"),),
+      body: GridView.count(
+  // Create a grid with 2 columns. If you change the scrollDirection to
+  // horizontal, this produces 2 rows.
+  crossAxisCount: 3,
+  // Generate 100 widgets that display their index in the List.
+
+  //child: ElevatedButton(onPressed: () {buttonPress_();}, child: Text("Click to add position "));
+  // return Center(child: Text(x_or_o),);
+  children: List.generate(9, (int index) {
+      // 
+      return GestureDetector(
+        onTap: () {buttonPress_(index);},
+        child: Container
+        (decoration: BoxDecoration  (border: Border.all(color: Colors.grey)),
+          child: Center( child: Text (x_or_o_Array[index]),
+        )  ),
+      );
+  }),
+),
+      );
   }
+
+  void buttonPress_(int index){ setState (() {x_or_o_Array[index] = "o";} );}
+
 }
 
 
